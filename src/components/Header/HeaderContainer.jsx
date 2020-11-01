fe5af313504a9831dac95842cbe0a12d73f18400
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-11-01 23:25:13 +0300

    Add comments

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './Header';
-
+// Тут место для оберток и логике, не относящейся напрямую к компоненте Header (например редиректы и тд).
 const HeaderContainer = () => {
     return (
         <Header />
