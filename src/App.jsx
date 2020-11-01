f9b65023fc1ef6c479bd90b3ec485f66032119ab
ReSenpai <57324282+ReSenpai@users.noreply.github.com>
2020-11-01 23:19:58 +0300

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { HashRouter, Redirect, Route, Switch } from 'react-router-dom';
 import { ReactComponent as Error404 } from './assets/icons/error_404.svg';
+import HeaderContainer from './components/Header/HeaderContainer';
 import TestComponent from './components/TestComponent/Test';
 
 function App() {
   return (
     <HashRouter>
+      <HeaderContainer />
       <Switch>
         <Route exact path="/" render={() => <Redirect to={'/game'} />} />
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Header = () => {
+    return (
+        <Wrapper>
+            <Container>
+                <NavLink to='/game-rules' activeClassName='active'>
+                    Правила игры
+                </NavLink>
+                <NavLink to='/watch-game' activeClassName='active'>
+                    Посмотреть игру
+                </NavLink>
+                <NavLink to='/contacts' activeClassName='active'>
+                    Контакты
+                </NavLink>
+            </Container>
+            <Container>
+                <NavLink to='/register' activeClassName='active'>
+                    Регистрация
+                </NavLink>
+                <NavLink to='/forgotten-password' activeClassName='active'>
+                    Забыли пароль?
+                </NavLink>
+            </Container>
+        </Wrapper>
+    )
+}
+
+export default Header;
+
+const Wrapper = styled.header`
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+    width: 100;
+    padding-top: 60px;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-weight: 400;
+    font-size: 18px;
+`
+
+const Container = styled.div`
+    display: flex;
+    a {
+        text-decoration: none;
+        padding-right: 30px;
+        &:link {
+            color: white;
+        }
+        &:visited {
+            color: white;
+        }
+        &.active {
+            color: red;
+            font-weight: 400;
+            font-size: 18px;
+        }
+    }
+
+    
+`
