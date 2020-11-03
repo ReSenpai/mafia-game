8ed80b91faf68e721dcfe31679469058c5244a7f
Alex <tictak211@gmail.com>
2020-11-03 13:34:20 +0200

    header removed

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import React from 'react';
 // === router ===
 import { Route, Switch } from 'react-router-dom';
 
-// === components ===
-import HeaderContainer from './components/Header/HeaderContainer';
-
 // === pages ===
 import {
   HomePage,
@@ -22,7 +19,6 @@ import {
 function App() {
   return (
     <>
-      <HeaderContainer />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/game-rules" component={GameRulesPage} />
