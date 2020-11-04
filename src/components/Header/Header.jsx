ea8c245e0df6b72cf99b11299c4ad84c1bb634a6
Alex <tictak211@gmail.com>
2020-11-04 14:37:28 +0200

    Header wrap fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,31 +2,35 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
+import Layout from '../Layout/Layout';
+
 import { colors } from '../../utils/variables';
 
 const Header = () => {
   return (
-    <Wrapper>
-      <Container>
-        <NavLink to="game-rules" activeClassName="active">
-          Правила игры
-        </NavLink>
-        <NavLink to="watch-game" activeClassName="active">
-          Посмотреть игру
-        </NavLink>
-        <NavLink to="contacts" activeClassName="active">
-          Контакты
-        </NavLink>
-      </Container>
-      <Container>
-        <NavLink to="register" activeClassName="active">
-          Регистрация
-        </NavLink>
-        <NavLink to="forgotten-password" activeClassName="active">
-          Забыли пароль?
-        </NavLink>
-      </Container>
-    </Wrapper>
+    <Layout>
+      <Wrapper>
+        <Container>
+          <NavLink to="game-rules" activeClassName="active">
+            Правила
+          </NavLink>
+          <NavLink to="watch-game" activeClassName="active">
+            Посмотреть игру
+          </NavLink>
+          <NavLink to="contacts" activeClassName="active">
+            Контакты
+          </NavLink>
+        </Container>
+        <Container>
+          <NavLink to="register" activeClassName="active">
+            Регистрация
+          </NavLink>
+          <NavLink to="forgotten-password" activeClassName="active">
+            Забыли пароль?
+          </NavLink>
+        </Container>
+      </Wrapper>
+    </Layout>
   );
 };
 
@@ -36,23 +40,17 @@ const { white, red } = colors;
 
 const Wrapper = styled.header`
   display: flex;
-  gap: 30px;
+  gap: 2em;
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
   display: flex;
-  padding-top: 60px;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  font-weight: 400;
-  font-size: 18px;
-  max-width: 1080px;
-  margin: auto;
 `;
 
 const Container = styled.div`
   display: flex;
-  gap: 30px;
+  flex-wrap: wrap;
+  gap: 2em;
 
   a {
     text-decoration: none;
