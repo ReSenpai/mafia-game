14ad83e3fab4bd2a1a73cb7d37306804d9acebfd
Alex <tictak211@gmail.com>
2020-11-04 13:24:10 +0200

    Logo component

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// === assets ===
+import TitleLogo from '../../assets/icons/MAFIA.svg';
+
+const Logo = ({ size }) => {
+  return (
+    <LogoWrapper size={size}>
+      <img src={TitleLogo} alt="Mafia logo" />
+    </LogoWrapper>
+  );
+};
+
+export default Logo;
+
+const LogoWrapper = styled.div`
+  width: 100%;
+  height: auto;
+  margin: 0 auto;
+  max-width: ${({ size }) => size || 'auto'};
+
+  img {
+    width: 100%;
+  }
+`;
