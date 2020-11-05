60e0facfeadf93c7dcf0e4979b3225f6f2720bd8
Alex <tictak211@gmail.com>
2020-11-05 18:42:47 +0200

    remove unused import

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ export { default as Page404 } from './404/404';
 export { default as GameRulesPage } from './GameRules/GameRules';
 export { default as WatchGamePage } from './WatchGame/WatchGame';
 export { default as ContactsPage } from './Contacts/Contacts';
-export { default as RegisterPage } from './Register/Register';
 export { default as ForgottenPasswordPage } from './ForgottenPassword/ForgottenPassword';
 export { default as GamePage } from './Game/Game';
 export { default as CurrentGamesPage } from './CurrentGames/CurrentGames';
