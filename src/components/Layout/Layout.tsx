5a71547b4a65105e18c332c85ab9a8293672489f
Alex <tictak211@gmail.com>
2020-11-10 17:57:49 +0200

    Layout component types

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,11 @@
+import React from 'react';
 import styled from 'styled-components';
 
-const Layout = ({ children }) => {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
   return <Wrapper>{children}</Wrapper>;
 };
 
